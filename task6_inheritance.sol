754e520da2fa56a70d0bb8889c01cd6ab29c3a7a
J Sarjeant <103888453+dev-jsarje@users.noreply.github.com>
2022-10-23 10:47:20 -0400

    Update task6_inheritance.sol

--- task6_inheritance.sol
+++ task6_inheritance.sol
@@ -1,1 +1,14 @@
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// A simple use case for inheritance is to launch an ERC20 token using the OpenZepellin ERC20 implementation.
+// Try to launch your custom ERC20 on any test network and then send me the address to the contract!
+
+
+contract TeamLHToken is ERC20 {
+    constructor() ERC20("TeamLHToken", "TLT") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
